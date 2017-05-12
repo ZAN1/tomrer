97dc4058fabca206d35e1761ba4409c4bf3edf64
Christian Frederiksen <caf80v2@gmail.com>
2017-05-12 21:40:36 +0200

    added 2 new pages

--- MyApplication/app/src/main/java/com/example/klisterdk/tomrer/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/klisterdk/tomrer/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.klisterdk.tomrer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
@@ -16,17 +17,28 @@ public class MainActivity extends AppCompatActivity {
         TextView handvarker = (TextView) findViewById(R.id.handvarker);
         TextView mester = (TextView) findViewById(R.id.mester);
 
+        mester.setOnClickListener(new View.OnClickListener(){
 
+        @Override
+                public void onClick(View v) {
+
+            Intent intent = new Intent(MainActivity.this, MesterActivity.class);
+
+            startActivity(intent);
+
+        }
+
+        });
         handvarker.setOnClickListener(new View.OnClickListener(){
 
             @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,TaskActivity.class);
 
-                    public onClick(View v){
-
-
-
-
+                startActivity(intent);
             }
+
+
 
 
 
@@ -38,9 +50,9 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-    public void onClick(){
 
 
 
-    }
+
+    
 }
---
+++ MyApplication/app/src/main/java/com/example/klisterdk/tomrer/MesterActivity.java
@@ -0,0 +1,13 @@
+package com.example.klisterdk.tomrer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MesterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mester);
+    }
+}
