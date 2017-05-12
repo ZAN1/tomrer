6de237ade01a41cb9c5136a4fcc55dc7535e4b6a
Christian Frederiksen <caf80v2@gmail.com>
2017-05-12 22:17:55 +0200

    need to make send button and implement calc and meterial list

---
+++ MyApplication/app/src/main/java/com/example/klisterdk/tomrer/Loft6060b.java
@@ -0,0 +1,13 @@
+package com.example.klisterdk.tomrer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Loft6060b extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loft6060b);
+    }
+}
--- MyApplication/app/src/main/java/com/example/klisterdk/tomrer/MesterActivity.java
+++ MyApplication/app/src/main/java/com/example/klisterdk/tomrer/MesterActivity.java
@@ -1,7 +1,10 @@
 package com.example.klisterdk.tomrer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MesterActivity extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class MesterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mester);
+
+
+        final TextView loft6060 = (TextView) findViewById(R.id.loft60x60);
+
+
+        loft6060.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MesterActivity.this,Loft6060b.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
     }
 }
