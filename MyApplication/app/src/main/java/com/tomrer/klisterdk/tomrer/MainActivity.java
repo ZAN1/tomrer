0bdff83767d2b4b07862d862c71395130821e39f
Christian Frederiksen <caf80v2@gmail.com>
2017-05-18 21:49:18 +0200

    rettet stig til mester siden

--- MyApplication/app/src/main/java/com/tomrer/klisterdk/tomrer/MainActivity.java
+++ MyApplication/app/src/main/java/com/tomrer/klisterdk/tomrer/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
                 public void onClick(View v) {
 
-            Intent intent = new Intent(MainActivity.this, MesterActivity.class);
+            Intent intent = new Intent(MainActivity.this, MesterCategory.class);
 
             startActivity(intent);
 
---
+++ MyApplication/app/src/main/java/com/tomrer/klisterdk/tomrer/MesterCategory.java
@@ -0,0 +1,26 @@
+package com.tomrer.klisterdk.tomrer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MesterCategory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mester_category);
+
+        TextView lofter = (TextView) findViewById(R.id.loft);
+
+            lofter.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MesterCategory.this,MesterActivity.class);
+                    startActivity(intent);
+                }
+            });
+    }
+}
