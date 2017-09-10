bdaf19ccf5afef7d54cd5e40df704b4ebaad02bf
Christian Frederiksen <caf80v2@gmail.com>
2017-09-10 13:23:50 +0200

    gipsloft inddeling klar i udregning mangler knap baggrund og toolbar

--- MyApplication/app/src/main/java/com/tomrer/klisterdk/tomrer/Gips/GipsLoftResult.java
+++ MyApplication/app/src/main/java/com/tomrer/klisterdk/tomrer/Gips/GipsLoftResult.java
@@ -7,6 +7,8 @@ import android.widget.TextView;
 import com.tomrer.klisterdk.tomrer.R;
 import com.tomrer.klisterdk.tomrer.get_set.GetSetGipsLoft;
 
+import static java.lang.Math.floor;
+
 public class GipsLoftResult extends AppCompatActivity {
 
     @Override
@@ -15,15 +17,42 @@ public class GipsLoftResult extends AppCompatActivity {
         setContentView(R.layout.activity_gips_loft_result);
 
         TextView gipsLoftPladetilkant = (TextView) findViewById(R.id.Loftpladetilkant);
+        TextView gipsLoftPladetilkant2 = (TextView) findViewById(R.id.Loftpladetilkant2);
         String gipsLoftlaengde = GetSetGipsLoft.getLaengde();
 
         double LoftlaengdeMeter = Double.parseDouble(gipsLoftlaengde);
         double Loftlaengde = LoftlaengdeMeter * 100;// går fra meter til cm
         double pladeStorelse = 90;
 
+        double antalPlader = Loftlaengde / pladeStorelse;
 
 
 
+           double kantPlade = (Loftlaengde - ((floor(antalPlader))*90)) / 2;
+
+        String kantPladetiltext1 = Double.toString(kantPlade);
+
+        gipsLoftPladetilkant.setText(kantPladetiltext1 + " cm");
+        gipsLoftPladetilkant2.setText(kantPladetiltext1 + " cm");
+
+            if(kantPlade == 0){
+               double kantPlade1 = (Loftlaengde - ((floor(antalPlader)-1))) / 2;
+                String kantPladetiltext =Double.toString(kantPlade1);
+                gipsLoftPladetilkant.setText(kantPladetiltext + " cm");
+                gipsLoftPladetilkant2.setText(kantPladetiltext + " cm");
+            }
+
+            if(kantPlade < 30){
+
+                 double unevenkantplade =  60 - kantPlade * 2 ;
+                       double newPlade = (kantPlade * 2) + unevenkantplade;
+                    double gipsplade = 90 - unevenkantplade;
+                    String kantPladetiltext = Double.toString(newPlade);
+                String kantPlade2tiltext = Double.toString(gipsplade);
+                    gipsLoftPladetilkant.setText(kantPladetiltext + " cm");
+                    gipsLoftPladetilkant2.setText(kantPlade2tiltext + " cm");
+
+            }
 
 
     }
